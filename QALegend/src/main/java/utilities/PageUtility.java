cce9806240cb76c0fad55dedd66ecb74114ad3a7
Akhila Anakha <Akhila Anakha@LAPTOP-GOCGSJGJ>
2024-06-21 07:42:33 +0530

    final

--- QALegend/src/main/java/utilities/PageUtility.java
+++ QALegend/src/main/java/utilities/PageUtility.java
@@ -1,6 +1,9 @@
 package utilities;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class PageUtility {
@@ -19,4 +22,46 @@ public class PageUtility {
 		Select select = new Select(element);
 		select.selectByIndex(value);	
 	}
+	public void clickAndHoldOnElement(WebElement element, WebDriver driver) {
+		Actions actions = new Actions(driver);
+		actions.clickAndHold(element).build().perform();
+	}
+	public void contextClick(WebElement element, WebDriver driver) {
+		Actions actions = new Actions(driver);
+		actions.contextClick(element).perform();
+	}
+	public void moveToElement(WebElement element, WebDriver driver) {
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element).perform();
+	}
+	public void doubleClick(WebElement element, WebDriver driver) {
+		Actions actions = new Actions(driver);
+		actions.doubleClick(element).perform();
+	}
+	
+	public String toGetAttributeValue(WebElement element,String attribute)
+	{
+		String attributeValue=element.getAttribute(attribute);
+		return attributeValue;
+	}
+
+	public void selectSingleDropdownValue(WebElement dropDown,String dropDownvalue)
+	{
+		Select selectsingledropdown=new Select(dropDown);
+		selectsingledropdown.selectByValue(dropDownvalue);
+	}
+    
+	public void selectDropdownValue(WebElement dropDown,String dropDownvalue)
+	{
+		Select selectsingledropdowntext=new Select(dropDown);
+		selectsingledropdowntext.selectByVisibleText(dropDownvalue);
+	}
+	public void selectDropdownByUsingIndex(WebElement element,int index)
+	{
+		Select select=new Select(element);
+		select.selectByIndex(index);
+	}
+
+	
+
 }
